2153c11b8a73777b42162d46c1c32adb2b96f755
Nikita Yupatov <hukull3@gmail.com>
2021-09-14 11:45:08 +0300

    Убрал слэш

--- src/view/film-view.js
+++ src/view/film-view.js
@@ -14,7 +14,7 @@ const getCardTemplate = (film) => {
       <span class="film-card__duration">${formatDate(createDurationMinutes(duration),DateFormats.HOURS_AND_MINUTES)}</span>
       <span class="film-card__genre">${genres[0]}</span>
     </p>
-    <img src="/${poster}" alt="" class="film-card__poster">
+    <img src="${poster}" alt="" class="film-card__poster">
     <p class="film-card__description">${shortDescription}</p>
     <a class="film-card__comments">${comments.length} comments</a>
     <div class="film-card__controls">
